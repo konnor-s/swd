ee91507e1df381fd00340d504a0e25956b4c4cad
kksommer <kksommer@uiowa.edu>
2020-09-08 15:36:59 -0500

    commenting code

--- oral_exam1/6-24_PerfectNumbers_Easy/src/Perfect.java
+++ oral_exam1/6-24_PerfectNumbers_Easy/src/Perfect.java
@@ -1,16 +1,34 @@
+/**
+ * Class which implements perfect number functions.
+ * @author Konnor Sommer
+ * @version %I%, %G%
+ *  */
+
 public class Perfect {
+    /**
+     * Tests whether an input number is perfect
+     * Returns a boolean indicating whether the number is perfect
+     * @param x     The number to test
+     * @return      True if number is perfect, else false
+     */
+
     public static boolean isPerfect(int x){
-        int sum=1;
-        for(int i=2;i<=Math.sqrt(x);i++){
+        int sum=1;//sum starts at 1
+        for(int i=2;i<=Math.sqrt(x);i++){//test for factors starting at 2 going to the numbers square root so that factors wont repeat. Excludes the number and 1.
             if(x%i==0){
                 sum+=(x/i+i);
             }
         }
-        return (sum==x) && (x!=1);
+        return (sum==x) && (x!=1);//Exclude 1 from perfect numbers
     }
+
+    /**
+     * Prints the factors of an input number
+     * @param x     The number of which to print factors
+     */
     public static void printFactors(int x){
-        System.out.printf("1");
-        for(int i=2;i<x;i++) {
+        System.out.printf("1");//first factor is 1
+        for(int i=2;i<x;i++) {//test for factors starting at 2 until x, printing each as it goes
             if (x % i == 0) {
                 System.out.printf(" + %d",i);
             }
--- oral_exam1/6-24_PerfectNumbers_Easy/src/PerfectTest.java
+++ oral_exam1/6-24_PerfectNumbers_Easy/src/PerfectTest.java
@@ -1,8 +1,18 @@
+/**
+ * Class which tests for perfect numbers between 1 and 10000
+ * Implements methods from class Perfect
+ * @author Konnor Sommer
+ * @version %I%, %G%
+ */
 public class PerfectTest{
+    /**
+     * Main function which tests for perfect numbers between 1 and 10000
+     * @param args
+     */
     public static void main(String[] args){
         System.out.print("Perfect numbers between 1 and 10,000:");
-        for(int i=1;i<=10000;i++)
-            if(Perfect.isPerfect(i)){
+        for(int i=1;i<=10000;i++)//test numbers between 1 and 10,000
+            if(Perfect.isPerfect(i)){//test whether the number is perfect. If it is, print it, then run the printFactors function to print its factors
                 System.out.printf("%n%d: ",i);
                 Perfect.printFactors(i);
             }
