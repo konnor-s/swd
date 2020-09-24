ffa32b00f59f1377e969de89e22611f1b1c38db9
kksommer <kksommer@uiowa.edu>
2020-09-24 11:03:33 -0500

    Trying to implement the hard part, it's not working yet

--- oral_exam1/S10_ArabicToRomanGUI_Hard/src/GUI.java
+++ oral_exam1/S10_ArabicToRomanGUI_Hard/src/GUI.java
@@ -1,15 +1,17 @@
 import java.awt.FlowLayout;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JOptionPane;
-
+import javax.swing.event.DocumentListener;
+import java.awt.event.TextEvent;
+import java.awt.event.TextListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
 public class GUI extends JFrame{
     private final JTextField textField1;
     private final JTextField textField2;
+
     //private final JTextField textField3;
     //private final JTextField textField4;
 
@@ -17,30 +19,55 @@ public class GUI extends JFrame{
         super("Convert Between Arabic and Roman Numerals");
         setLayout(new FlowLayout());
 
-        textField1=new JTextField(10);
-        textField1.setText("Enter Roman Numerals");
+        textField1=new JFormattedTextField("XV");
+        //textField1.setText("XV");
         add(textField1);
-        textField2=new JTextField(10);
-        textField2.setEditable(false);
+        textField2=new JFormattedTextField("15");
+        //textField2.setText("15")
         add(textField2);
         //textField3=new JTextField(10);
         //add(textField3);
         //textField4=new JTextField(10);
        // add(textField4);
-
-        TextFieldHandler handler = new TextFieldHandler();
-        textField1.addActionListener(handler);
+        FieldHandler handler = new FieldHandler();
+        textField1.addPropertyChangeListener("value",handler);
+        textField2.addPropertyChangeListener("value", handler);
     }
-    private class TextFieldHandler implements ActionListener{
+    /*
+    private class TextFieldHandler implements TextListener{
         @Override
-        public void actionPerformed(ActionEvent event){
+        public void textValueChanged(TextEvent event){
             String string = "";
             int num = 0;
 
             if (event.getSource() == textField1){
-                string = event.getActionCommand();
+                string = event.paramString();
                 num = Converter.RomanToArabic(string);
                 textField2.setText(Integer.toString(num));
+            }
+            if(event.getSource() == textField2){
+                string = event.paramString();
+                string = Converter.ArabicToRoman(Integer.parseInt(string));
+                textField1.setText(string);
+            }
+        }
+
+     */
+    public class FieldHandler implements PropertyChangeListener{
+        @Override
+        public void propertyChange(PropertyChangeEvent event) {
+            String string = "";
+            int num = 0;
+
+            if (event.getSource() == textField1) {
+                string = event.getPropertyName();
+                num = Converter.RomanToArabic(string);
+                textField2.setText(Integer.toString(num));
+            }
+            else if (event.getSource() == textField2) {
+                string = event.getPropertyName();
+                string = Converter.ArabicToRoman(Integer.parseInt(string));
+                textField1.setText(string);
             }
         }
     }
--- oral_exam1/S10_ArabicToRomanGUI_Hard/src/TestArabicToRoman.java
+++ oral_exam1/S10_ArabicToRomanGUI_Hard/src/TestArabicToRoman.java
@@ -4,7 +4,7 @@ public class TestArabicToRoman {
     public static void main(String[] args){
         GUI gui = new GUI();
         gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        gui.setSize(350,100);
+        gui.setSize(500,200);
         gui.setVisible(true);
 
 
