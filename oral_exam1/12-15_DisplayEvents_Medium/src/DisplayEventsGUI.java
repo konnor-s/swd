aad0c7cc7d7279b8be340ece8ed3b7aea66ba25c
kksommer <kksommer@uiowa.edu>
2020-10-01 14:59:48 -0500

    Implementing all events into working gui

--- oral_exam1/12-15_DisplayEvents_Medium/src/DisplayEventsGUI.java
+++ oral_exam1/12-15_DisplayEvents_Medium/src/DisplayEventsGUI.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.event.*;
 import javax.swing.JLabel;
+import javax.swing.border.Border;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.event.ListSelectionEvent;
 import java.awt.*;
@@ -15,10 +16,19 @@ KeyListener
  */
 public class DisplayEventsGUI extends JFrame{
     private final JTextField inputField;
+    private final JLabel actionCap;
+    private final JLabel keyCap;
+    private final JLabel itemCap;
+    private final JLabel listCap;
+    private final JLabel mouseCap;
+    private final JLabel motionCap;
     private final JLabel actionField;
     private final JLabel keyField;
     private final JLabel itemField;
     private final JLabel listField;
+    private final JLabel mouseField;
+    private final JLabel motionField;
+    private final JLabel instruct;
     private final JList<String> colorList;
     private final ButtonGroup buttons;
     private final JRadioButton plainButton;
@@ -26,22 +36,50 @@ public class DisplayEventsGUI extends JFrame{
     private final Font labelFont;
     private final Color[] colors={Color.WHITE,Color.BLUE,Color.RED};
     private final String[] colorNames={"White","Blue","Red"};
+    private final JPanel panel1;
+    private final JPanel panel2;
     public DisplayEventsGUI() {
         super("Display Events");
-        setLayout(new FlowLayout());
+        panel1=new JPanel();
+        panel1.setLayout(new FlowLayout());
+        panel2=new JPanel();
+        panel2.setLayout(new GridLayout(12,1));
         colorList=new JList<String>(colorNames);
         colorList.setVisibleRowCount(3);
         colorList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
         labelFont=new Font("Serif",Font.PLAIN,9);
         inputField = new JTextField(15);
+        instruct=new JLabel();
+        instruct.setText("Move/click mouse, use list, use buttons, and type to see events");
+
+
+        actionCap=new JLabel();
+        actionCap.setText("Action Event:");
         actionField = new JLabel();
         actionField.setFont(labelFont);
+        keyCap=new JLabel();
+        keyCap.setText("Key Event:");
         keyField = new JLabel();
         keyField.setFont(labelFont);
+        itemCap=new JLabel();
+        itemCap.setText("Item Event:");
         itemField=new JLabel();
         itemField.setFont(labelFont);
+        listCap=new JLabel();
+        listCap.setText("List Selection Event:");
         listField=new JLabel();
         listField.setFont(labelFont);
+        mouseCap=new JLabel();
+        mouseCap.setText("Mouse Event:");
+        mouseField=new JLabel();
+        mouseField.setFont(labelFont);
+        motionCap=new JLabel();
+        motionCap.setText("Mouse Motion Event:");
+        motionField=new JLabel();
+        motionField.setFont(labelFont);
+
+
 
         plainButton = new JRadioButton("Plain", true);
         italicButton = new JRadioButton("Italic", false);
@@ -49,14 +87,29 @@ public class DisplayEventsGUI extends JFrame{
         buttons.add(plainButton);
         buttons.add(italicButton);
 
-        add(new JScrollPane(colorList));
-        add(inputField);
-        add(plainButton);
-        add(italicButton);
-        add(actionField);
-        add(keyField);
-        add(itemField);
-        add(listField);
+
+
+        panel1.add(instruct);
+        panel1.add(new JScrollPane(colorList));
+        panel1.add(inputField);
+        panel1.add(plainButton);
+        panel1.add(italicButton);
+
+        panel2.add(actionCap);
+        panel2.add(actionField);
+        panel2.add(keyCap);
+        panel2.add(keyField);
+        panel2.add(itemCap);
+        panel2.add(itemField);
+        panel2.add(listCap);
+        panel2.add(listField);
+        panel2.add(mouseCap);
+        panel2.add(mouseField);
+        panel2.add(motionCap);
+        panel2.add(motionField);
+
+        add(panel1,BorderLayout.NORTH);
+        add(panel2,BorderLayout.SOUTH);
 
         plainButton.addItemListener(new ButtonHandler(new Font("Serif",Font.PLAIN,14)));
         italicButton.addItemListener(new ButtonHandler(new Font("Serif",Font.ITALIC,14)));
@@ -65,7 +118,7 @@ public class DisplayEventsGUI extends JFrame{
                 new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent actionEvent) {
-                        actionField.setText(actionEvent.toString());
+                        actionField.setText("Action Event: " + actionEvent.toString()+"\n");
                     }
                 }
         );
@@ -98,6 +151,47 @@ public class DisplayEventsGUI extends JFrame{
                     }
                 }
         );
+        addMouseListener(
+                new MouseListener() {
+                    @Override
+                    public void mouseClicked(MouseEvent mouseEvent) {
+                        mouseField.setText(mouseEvent.toString()+"\n");
+                    }
+
+                    @Override
+                    public void mousePressed(MouseEvent mouseEvent) {
+                        mouseField.setText(mouseEvent.toString()+"\n");
+                    }
+
+                    @Override
+                    public void mouseReleased(MouseEvent mouseEvent) {
+                        mouseField.setText(mouseEvent.toString()+"\n");
+                    }
+
+                    @Override
+                    public void mouseEntered(MouseEvent mouseEvent) {
+                        mouseField.setText(mouseEvent.toString()+"\n");
+                    }
+
+                    @Override
+                    public void mouseExited(MouseEvent mouseEvent) {
+                        mouseField.setText(mouseEvent.toString()+"\n");
+                    }
+                }
+        );
+        addMouseMotionListener(
+                new MouseMotionListener(){
+                    @Override
+                    public void mouseDragged(MouseEvent mouseEvent) {
+                        motionField.setText(mouseEvent.toString()+"\n");
+                    }
+
+                    @Override
+                    public void mouseMoved(MouseEvent mouseEvent) {
+                        motionField.setText(mouseEvent.toString()+"\n");
+                    }
+                }
+        );
     }
     private class ButtonHandler implements ItemListener{
         private Font font;
@@ -108,7 +202,7 @@ public class DisplayEventsGUI extends JFrame{
         @Override
         public void itemStateChanged(ItemEvent event){
             inputField.setFont(font);
-            itemField.setText(event.toString());
+            itemField.setText(event.toString()+"\n");
         }
     }
 }
--- oral_exam1/12-15_DisplayEvents_Medium/src/TestDisplayEvents.java
+++ oral_exam1/12-15_DisplayEvents_Medium/src/TestDisplayEvents.java
@@ -4,7 +4,7 @@ public class TestDisplayEvents {
     public static void main(String[] args){
         DisplayEventsGUI gui = new DisplayEventsGUI();
         gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        gui.setSize(1200,500);
+        gui.setSize(1300,300);
         gui.setVisible(true);
     }
 }
