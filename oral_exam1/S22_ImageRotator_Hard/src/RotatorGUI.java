48a29f581b1e50097b5c76307d394250a5ae5874
kksommer <kksommer@uiowa.edu>
2020-10-07 10:08:51 -0500

    Added spin speed slider.

--- oral_exam1/S22_ImageRotator_Hard/src/RotatorGUI.java
+++ oral_exam1/S22_ImageRotator_Hard/src/RotatorGUI.java
@@ -1,56 +1,74 @@
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
 
 
 public class RotatorGUI extends JFrame{
+
     private final RotateLabel rLabel;
-    private final JTextField speedField;
     private final JTextField angleField;
     private int angle;
     private Timer timer;
     private final JRadioButton continuous;
     private final JRadioButton incremental;
-    private final ButtonGroup buttons;
-    private final JPanel topPanel;
+    private final JSlider speedSlider;
 
     public RotatorGUI(){
         angle=0;
-        speedField = new JTextField(15);
-        speedField.addActionListener(new SpeedHandler());
-        angleField = new JTextField(15);
-        angleField.addActionListener(new AngleHandler());
-        Icon pic = new ImageIcon(getClass().getResource("pic.png"));
+        angleField = new JTextField(5);
+
+        speedSlider = new JSlider(JSlider.HORIZONTAL, 0, 90, 45);
+        speedSlider.setMajorTickSpacing(15);
+        Hashtable labelTable = new Hashtable();
+        labelTable.put(  0 , new JLabel("0 deg/sec") );
+        labelTable.put( 90 , new JLabel("90 deg/sec") );
+        speedSlider.setLabelTable( labelTable );
+        speedSlider.setPaintLabels(true);
+        speedSlider.setPaintTicks(true);
+
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(getClass().getResource("pic.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Icon pic = new ImageIcon(img);
         rLabel = new RotateLabel(pic);
         add(rLabel);
 
-        topPanel = new JPanel();
-        topPanel.setLayout(new GridLayout(1,6));
-
-        continuous = new JRadioButton("Continuous", false);
-        incremental = new JRadioButton("Incremental", true);
-        buttons = new ButtonGroup();
+        continuous = new JRadioButton("Continuous Mode -- Use speed slider", false);
+        incremental = new JRadioButton("Incremental Mode -- Enter angle in box", true);
+        ButtonGroup buttons = new ButtonGroup();
         buttons.add(continuous);
         buttons.add(incremental);
 
-        topPanel.add(speedField);
-        topPanel.add(continuous);
-        topPanel.add(incremental);
-        topPanel.add(angleField);
+        JPanel infoPanel = new JPanel();
+        infoPanel.setLayout(new GridLayout(1,6));
+        infoPanel.add(speedSlider);
+        infoPanel.add(continuous);
+        infoPanel.add(incremental);
+        infoPanel.add(angleField);
+        add(infoPanel, BorderLayout.SOUTH);
 
-        add(topPanel, BorderLayout.SOUTH);
-
+        speedSlider.addChangeListener(new SliderHandler());
         continuous.addItemListener(new ButtonHandler());
         incremental.addItemListener(new ButtonHandler());
-
+        angleField.addActionListener(new AngleHandler());
 
         timer = new Timer(100, new TimerHandler());
     }
-
     private class RotateLabel extends JLabel {
         private int newAngle;
         public RotateLabel(Icon pic) {
@@ -65,8 +83,7 @@ public class RotatorGUI extends JFrame{
             Graphics2D g2d = (Graphics2D) g;
             g2d.rotate(Math.toRadians(newAngle),getX() + getWidth()/2.  ,getY() +getHeight()/2. );
             super.paintComponent(g2d);
-
-        }//
+        }
     }
     private class TimerHandler implements ActionListener {
         @Override
@@ -74,12 +91,6 @@ public class RotatorGUI extends JFrame{
             rLabel.setAngle(angle);
             rLabel.repaint();
             angle = angle + 2;
-        }
-    }
-    private class SpeedHandler implements ActionListener{
-        @Override
-        public void actionPerformed(ActionEvent actionEvent){
-            timer.setDelay(Integer.parseInt(speedField.getText()));
         }
     }
     private class AngleHandler implements ActionListener{
@@ -95,9 +106,20 @@ public class RotatorGUI extends JFrame{
         public void itemStateChanged(ItemEvent itemEvent) {
             if (continuous.isSelected()){
                 timer.start();
+                timer.setDelay(1000/(speedSlider.getValue()/2));
             }
             if (incremental.isSelected()){
                 timer.stop();
+            }
+        }
+    }
+    private class SliderHandler implements ChangeListener{
+
+        @Override
+        public void stateChanged(ChangeEvent changeEvent) {
+            if (continuous.isSelected()) {
+                int speed = speedSlider.getValue();
+                timer.setDelay(1000 / (speed / 2));
             }
         }
     }
--- oral_exam1/S22_ImageRotator_Hard/src/TestRotate.java
+++ oral_exam1/S22_ImageRotator_Hard/src/TestRotate.java
@@ -5,7 +5,7 @@ public class TestRotate {
     public static void main(String[] args){
         RotatorGUI gui = new RotatorGUI();
         gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        gui.setSize(1200,500);
+        gui.setSize(1200,800);
         gui.setVisible(true);
     }
 }
