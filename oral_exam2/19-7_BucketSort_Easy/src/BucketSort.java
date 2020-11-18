aab3ce5a14330d5f58b544572e73e5a956725747
kksommer <kksommer@uiowa.edu>
2020-11-18 09:49:45 -0600

    making basic class structure

---
+++ oral_exam2/19-7_BucketSort_Easy/src/BucketSort.java
@@ -0,0 +1,12 @@
+public class BucketSort {
+    int sortArray[];
+    int bucketArray[][];
+    BucketSort(int x[]){
+        int n = x.length;
+        sortArray = x;
+        bucketArray = new int[10][n-1];
+    }
+    public int[] sort(){
+
+    }
+}
