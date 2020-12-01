03163990c451d8cdc0f6d3a242a90e4e6afbca8d
kksommer <kksommer@uiowa.edu>
2020-12-01 08:45:18 -0600

    create random array

--- oral_exam2/S18_MergeSort_Medium/src/SortTest.java
+++ oral_exam2/S18_MergeSort_Medium/src/SortTest.java
@@ -1,5 +1,13 @@
+import java.util.Random;
+
 public class SortTest {
     public static void main(String args[]){
+        Random rand = new Random();
+        int[] testArray = new int[100];
+        for(int i = 0; i<100; i++){
+            testArray[i] = rand.nextInt(1000);
+            System.out.println(testArray[i]);
+        }
 
     }
 }
